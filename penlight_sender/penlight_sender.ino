4adc95806f485628ada0b6b08db16e6e75e5683c
iwamoto <iwamoto@cps.im.dendai.ac.jp>
2017-01-19 16:20:42 +0900

    save

--- penlight_sender/penlight_sender.ino
+++ penlight_sender/penlight_sender.ino
@@ -1,71 +1,99 @@
 #include <Adafruit_NeoPixel.h>
-String color = "default";
+#define ID 0
+#define NEOPIXEL 5
+#define button_A 6
+#define button_B 7
+
+#define LED 10
+
+//加速度
 int accel_x = 0;
 int accel_y = 0;
 int accel_z = 0;
 
-unsigned char buttonState = 0;
-const int neopixel = 5;
-const int button_a = 6;
-const int button_b = 7;
+//振り判定
+int shaked = 0;
+
+//一時保存
+int spped_log = 0;
+
+//ボタンの状態
+unsigned char buttonStateA = 0;
+unsigned char buttonStateB = 0;
+unsigned char backState = 0;
+
+//スイッチのカウント数
 unsigned char switchCount = 1;
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, neopixel, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
-char trans[35]; // 数字（文字列）の送信用配列
+char trans[50]; // 数字（文字列）の送信用配列
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(neopixel, OUTPUT);
-  pinMode(button_a, INPUT_PULLUP);
-  pinMode(button_b, INPUT_PULLUP);
-
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-
-  for (int i = 0; i > 12; i--) {
-    strip.setPixelColor(i, strip.Color(0, 0, 0));
-  }
-  strip.show();
-  Serial.println(switchCount);
+  pinMode(NEOPIXEL, OUTPUT);
+  pinMode(button_A, INPUT_PULLUP);
+  pinMode(button_B, INPUT_PULLUP);
+  neopixelColorBegin(LED);
   switchCount = 0;
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  accel_x = analogRead(A0);
+  accel_y = analogRead(A1);
+  accel_z = analogRead(A2);
 
-  accel_x = analogRead(A3);
-  accel_y = analogRead(A4);
-  accel_z = analogRead(A5);
+  //振り判定------------------------------------------
+  int s = shake(accel_x,accel_y,accel_z);
+  if(s > 900 || s < 800){
+    shaked = 1;
+  }
+  else {
+    shaked = 0;
+  }
 
-  //ボタン,Neopixel処理------------------------------------------
-  buttonState = digitalRead(button_a);
+  //ボタン処理------------------------------------------
+  buttonStateA = digitalRead(button_A);
+  buttonStateB = digitalRead(button_B);
 
-  if (buttonState == HIGH)
+  if (buttonStateA == 0)
   {
     switchCount += 1;
-//    Serial.println(color);
-    if (switchCount > 11) {
+    if (switchCount > 9) {
             switchCount = 0;
     }
   }
+  if (buttonStateB == 0)
+  {
+    if (switchCount == 0 ) {
+            switchCount = 10;
+    }
+    switchCount -= 1;
+  }
+  //--------------------------------------------------
+  neopixelColorChange(switchCount,LED);
+  
+  //加速度センサーの処理-----------------------------------------
+  char *json = &trans[0];
+  sprintf(json, "{\"id\":%d,\"x\":%d,\"y\":%d,\"z\":%d,\"color\":%d,\"shake\":%d}", ID, accel_x, accel_y, accel_z, switchCount,shaked);
+  int spped = (accel_x + accel_y + accel_z)/3;
+    Serial.println(json);
+    spped_log = spped;
+  delay(200);
+}
 
-  //  for(int i=0;i<12;i++){
-  //    if(switchCount==0){
-  //       strip.setPixelColor(i,strip.Color(255, 0, 0));
-  //    }
-  //    else if(switchCount==1){
-  //       strip.setPixelColor(i,strip.Color(0, 255, 0));
-  //    }
-  //    else{
-  //       strip.setPixelColor(i,strip.Color(0, 0, 255));
-  //    }
-  //  }
+void neopixelColorBegin(int led){
+  strip.begin();
+  for (int i = 0; i > led; i--) {
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+  }
+  strip.show();
+}
 
-  for (int i = 0; i < 12; i++) {
-    switch (switchCount) {
+void neopixelColorChange(int count,int led){
+  char color ="default";
+  for (int i = 0; i < led; i++) {
+    switch (count) {
       case 0:
         strip.setPixelColor(i, strip.Color(255, 0, 0));
         color = "red";
@@ -79,7 +107,7 @@ void loop() {
         color = "white";
         break;
       case 3:
-        strip.setPixelColor(i, strip.Color(0, 0, 0));
+        strip.setPixelColor(i, strip.Color(255, 165, 0));
         color = "orange";
         break;
       case 4:
@@ -91,11 +119,11 @@ void loop() {
         color = "purple";
         break;
       case 6:
-        strip.setPixelColor(i, strip.Color(0, 0, 0));
+        strip.setPixelColor(i, strip.Color(255, 100, 100));
         color = "light_pink";
         break;
       case 7:
-        strip.setPixelColor(i, strip.Color(0, 255, 255));
+        strip.setPixelColor(i, strip.Color(255, 255, 0));
         color  = "yellow";
         break;
       case 8:
@@ -107,7 +135,7 @@ void loop() {
         color = "light_blue";
         break;
       case 10:
-        strip.setPixelColor(i, strip.Color(255, 100, 100));
+        strip.setPixelColor(i, strip.Color(255, 50, 50));
         color =  "pink";
         break;
       case 11:
@@ -118,17 +146,10 @@ void loop() {
         break;
     }
   }
-
-
-  //  Serial.print(switchCount);
-  strip.show();
-  //----------------------------------------------------------
-
-
-  //加速度センサーの処理-----------------------------------------
-  char *json = &trans[0];
-  sprintf(json, "{\"x\":%d,\"y\":%d,\"z\":%d,\"color\":%x}", accel_x, accel_y, accel_z, switchCount);
-  Serial.println(json);
-  //----------------------------------------------------------
-  delay(200);
+strip.show();
 }
+
+float shake(int x,int y,int z){
+  return sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+}
+
